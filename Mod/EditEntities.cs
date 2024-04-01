915cd41a5195f67e48eab7291aa44f8ae14215bf
kosch104 <konstantin-schwarze@outlook.de>
2024-04-01 17:07:47 +0200

    Initial commit

---
+++ Mod/EditEntities.cs
@@ -0,0 +1,59 @@
+﻿using Extra.Lib.Helper;
+using Extra.Lib;
+using Game.Prefabs;
+using Unity.Collections;
+using Unity.Entities;
+namespace ExtraAssetIcons
+{
+	internal static class EditEntities
+	{
+		internal static void SetupEditEntities()
+		{
+			EntityQueryDesc spawnableBuildingEntityQueryDesc = new EntityQueryDesc
+			{
+				Any =
+				[
+					ComponentType.ReadOnly<SpawnableBuildingData>(),
+					ComponentType.ReadOnly<PlaceholderBuildingData>()
+				]
+			};
+
+			ExtraLib.AddOnEditEnities(new(OnEditSpawnableBuildingEntities, spawnableBuildingEntityQueryDesc));
+		}
+
+		private static void Log(string message)
+		{
+			EAI.Logger.Info(message);
+		}
+
+		private static void OnEditSpawnableBuildingEntities(NativeArray<Entity> entities)
+		{
+			int i = 1;
+			foreach (Entity entity in entities)
+			{
+				//ENA.Logger.Info("Spawnable Building: " + i);
+				if (ExtraLib.m_PrefabSystem.TryGetPrefab(entity, out ObjectPrefab prefab))
+				{
+					//ENA.Logger.Info("Building " + i + " parsed. Prefab: " + prefab);
+					var prefabUI = prefab.GetComponent<UIObject>();
+					if (prefabUI == null)
+					{
+						prefabUI = prefab.AddComponent<UIObject>();
+						prefabUI.active = true;
+						prefabUI.m_IsDebugObject = false;
+						prefabUI.m_Icon = Icons.GetIcon(prefab, false);
+						prefabUI.m_Priority = 1;
+
+					}
+					//prefabUI.m_Group?.RemoveElement(entity);
+					prefabUI.m_Group = PrefabsHelper.GetOrCreateUIAssetCategoryPrefab("Landscaping", "Spawnable Buildings", Icons.GetIcon);
+					prefabUI.m_Group.AddElement(entity);
+
+					ExtraLib.m_EntityManager.AddOrSetComponentData(entity, prefabUI.ToComponentData());
+				}
+
+				i++;
+			}
+		}
+	}
+}
---
+++ Mod/Icons.cs
@@ -0,0 +1,63 @@
+﻿using Game.Prefabs;
+using System.IO;
+
+namespace ExtraAssetIcons
+{
+    internal class Icons
+    {
+        internal const string IconsResourceKey = "ExtraAssetIcons";
+        internal static readonly string COUIBaseLocation = $"coui://{IconsResourceKey}";
+
+        public static readonly string Placeholder = $"{COUIBaseLocation}/Icons/Misc/placeholder.svg";
+        public static readonly string GameCrashWarning = $"{COUIBaseLocation}/Icons/Misc/GameCrashWarning.svg";
+
+        internal static void LoadIcons(string path)
+        {
+            Extra.Lib.UI.Icons.LoadIconsFolder(IconsResourceKey, path);
+        }
+
+        public static string GetIcon(PrefabBase prefab)
+        {
+            return GetIcon(prefab, true);
+        }
+
+        public static string GetIcon(PrefabBase prefab, bool replaceWithPlaceholder = true)
+        {
+
+            if (prefab is null)
+                if (replaceWithPlaceholder)
+                    return $"{COUIBaseLocation}/Icons/Misc/placeholder.svg";
+                else
+                    return null;
+
+            if (File.Exists($"{EAI.ResourcesIcons}/{prefab.GetType().Name}/{prefab.name}.svg"))
+            {
+                //ENA.Logger.Info($"Found icon in mod folder: {ENA.ResourcesIcons}/{prefab.GetType().Name}/{prefab.name}.svg");
+                return $"{COUIBaseLocation}/Icons/{prefab.GetType().Name}/{prefab.name}.svg";
+            }
+            if (File.Exists($"{EAI.ResourcesIcons}/{prefab.GetType().Name}/{prefab.name}.png"))
+            {
+                return $"{COUIBaseLocation}/Icons/{prefab.GetType().Name}/{prefab.name}.png";
+            }
+
+            if (!replaceWithPlaceholder)
+                return null;
+
+            //ENA.Logger.Info($"Did not find icon in mod folder: {ENA.ResourcesIcons}/{prefab.GetType().Name}/{prefab.name}.svg");
+
+            if (prefab is UIAssetCategoryPrefab)
+            {
+
+                return $"{COUIBaseLocation}/Icons/Misc/placeholder.svg";
+            }
+            else if (prefab is UIAssetMenuPrefab)
+            {
+
+                return $"{COUIBaseLocation}/Icons/Misc/placeholder.svg";
+            }
+
+            return Placeholder;
+        }
+
+    }
+}
